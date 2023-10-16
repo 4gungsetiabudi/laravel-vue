3c67fe3006a331637e04572915707704cb5b7fd9
4gungsetiabudi <4gungsetiabudi@gmail.com>
2023-10-16 08:05:35 +0700

    Compact Data

---
+++ library/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/catalogs', [App\Http\Controllers\CatalogController::class, 'index']);
+Route::get('/publishers', [App\Http\Controllers\PublisherController::class, 'index']);
+Route::get('/authors', [App\Http\Controllers\AuthorController::class, 'index']);
+Route::get('/books', [App\Http\Controllers\BookController::class, 'index']);
+Route::get('/members', [App\Http\Controllers\MemberController::class, 'index']);
+
+
